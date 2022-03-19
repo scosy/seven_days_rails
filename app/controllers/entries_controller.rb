dace8462c54607a0e0e86221c2069d956ff38d6f
scosy <amine.bentaleb064@gmail.com>
2022-03-19 15:03:45 +0100

    Added create controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,11 +1,26 @@
 class EntriesController < ApplicationController
 	def index
 		@entries = JournalEntry.all
+		@entries = @entries.order(:created_at)
+
+		@today = Date.today
 	end
 
 	def new
 	end
 
 	def edit
+	end
+
+	def create
+		permitted_params = params.permit(:field_1, :field_2, :field_3, :field_4, :field_5, :field_6)
+
+		@entry = JournalEntry.create(permitted_params)
+
+		if @entry.persisted?
+			redirect_to entries_path
+		else
+			render :new
+		end
 	end
 end
--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -1,2 +1,3 @@
 class JournalEntry < ApplicationRecord
+	validates_presence_of :field_1, :field_2, :field_3
 end
