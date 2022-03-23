ac53dec0e6f4bad8afcd2ec9a49e5bb6c40b9f33
scosy <amine.bentaleb064@gmail.com>
2022-03-23 17:59:03 +0100

    Edit now working'

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,9 +7,13 @@ class EntriesController < ApplicationController
 	end
 
 	def new
+		@today = Date.today
 	end
 
 	def edit
+		@entry = JournalEntry.find(params[:id])
+		
+		@today = @entry.created_at
 	end
 
 	def create
@@ -23,4 +27,22 @@ class EntriesController < ApplicationController
 			render :new
 		end
 	end
+
+  def update
+    # Process the incoming data
+    permitted_params = params.require(:journal_entry).permit(:field_1, :field_2, :field_3, :field_4, :field_4, :field_5, :field_6)
+    # params = {"field_1"=>"", "field_2"=>"", "field_3"=>"", "field_4"=>"", "field_5"=>"", "field_6"=>""}
+    # Find the existing journal entry in the database
+    @entry = JournalEntry.find(params[:id]) #params[:id] is pulled from the URL
+    # We update our entry, and if no errors, redirect to entries_path
+    if @entry.update(permitted_params) # this line saves the record and then checks if it went well.
+    # If so, it redirects to index page if the creation was successful
+      redirect_to entries_path # You can find this helper by executing the "rails routes" command in your terminal
+    else
+    # Refresh the page and specifiy an error if the creation was unsuccesful
+      @day = @entry.created_at
+      render :edit # This is the simplest way to reload the page, while keeping our entries
+    end
+  end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :entries
+  patch 'entries', controller: :entries, action: :update, as: :journal_entry
   # Defines the root path route ("/")
   # root "articles#index"
 end
